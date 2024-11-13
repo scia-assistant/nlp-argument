3975d17bdfac0321038918538d6a61f1e425a68f
Sacha Hibon <75496318+Nedartemis@users.noreply.github.com>
2024-11-13 14:13:03 +0100

    5 load llm models (#15)
    * feat: load GPT2 LLMs
    
    * refacto: rename dir load_models to load_llm
    
    * feat: add eval_perplexity.py
    
    * fix(load_llm): renaming + add nb_parameters field
    
    * feat: load flan and gpt neo LLMs
    
    * feat: plot LLMs performances
    
    * feat: flan and gpt neo LLMs
    
    * chore: ignore a mypy error
    
    * chore: update requirements.txt
    
    * chore: fix documentation
    
    * fix: dependancies
    
    * fix(pylint):
    
    * feat: add TinyLlama + pythia + OLMo

---
+++ src/eval_llm/eval_perplexity.py
@@ -0,0 +1,89 @@
+"""Module that contains the evaluation of the perplexity"""
+
+from typing import List, Union
+
+import torch
+from tqdm import tqdm
+from transformers import logging
+
+
+def eval_perplexity(  # pylint: disable=too-many-locals
+    tokenizer,
+    model,
+    max_context_length,
+    text_input: Union[str, List],
+    device: str = "cpu",
+) -> float:
+    """Given a text and a LLM, compute its perplexity.
+
+    Args:
+        tokenizer (_type_): The llm tokenizer
+        model (_type_): The llm model
+        max_context_length (_type_): The max text length the llm has been trained with.
+        text_input (Union[str, List]): The text which will be use to compute the perplexity.
+        device (str, optional): Which device is used. Defaults to "cpu".
+        verbose (bool, optional): Display advancement of the pipeline if sets to True.
+        Defaults to True.
+
+    Returns:
+        float: The perplexity
+    """
+
+    text_str: str = (
+        text_input if isinstance(text_input, str) else "\n\n".join(text_input)
+    )
+
+    logging.set_verbosity_error()
+    encodings = tokenizer(text_str, return_tensors="pt")
+    logging.set_verbosity_warning()
+
+    stride = max_context_length
+    seq_len = encodings.input_ids.size(1)
+
+    nlls = []
+    prev_end_loc = 0
+    for begin_loc in tqdm(range(0, seq_len, stride), desc="Compute perplexity"):
+        end_loc = min(begin_loc + max_context_length, seq_len)
+        trg_len = end_loc - prev_end_loc  # may be different from stride on last loop
+        input_ids = encodings.input_ids[:, begin_loc:end_loc].to(device)
+        target_ids = input_ids.clone()
+        target_ids[:, :-trg_len] = -100
+
+        with torch.no_grad():
+            outputs = model(input_ids, labels=target_ids)
+
+            # loss is calculated using CrossEntropyLoss which averages over valid labels
+            # N.B. the model only calculates loss over trg_len - 1 labels,
+            # because it internally shifts the labels
+            # to the left by 1.
+            neg_log_likelihood = outputs.loss
+
+        nlls.append(neg_log_likelihood)
+
+        prev_end_loc = end_loc
+        if end_loc == seq_len:
+            break
+
+    ppl = torch.exp(torch.stack(nlls).mean())
+    return ppl.item()
+
+
+def _main():
+    from load_llm.load_llm import (  # pylint: disable=import-outside-toplevel
+        LLMPretrained,
+        LLMWrapper,
+    )
+
+    model = LLMWrapper(llm_pretrained=LLMPretrained.GPT2_SMALL)
+
+    ppl = eval_perplexity(
+        tokenizer=model.tokenizer,
+        model=model.model,
+        max_context_length=model.get_max_context_length(),
+        text_input="Once upon a time",
+    )
+    print("Perplexity :", ppl)
+
+
+if __name__ == "__main__":
+    _main()
