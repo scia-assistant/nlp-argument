a858ceee675c9f4e6df2d0ba68c5d724dfe73f4a
Lea Margery <margerylea@gmail.com>
2024-11-14 23:34:55 +0100

    feat(rag): test with one model

--- src/load.py
+++ src/load.py
@@ -5,16 +5,20 @@ from datasets import load_dataset
 from langchain.docstore.document import Document as LangchainDocument
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from sentence_transformers import SentenceTransformer
-from transformers import AutoTokenizer
+from transformers import AutoTokenizer, AutoModelForCausalLM, BitsAndBytesConfig
 import pandas as pd
 import matplotlib.pyplot as plt
+import torch
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain_community.vectorstores.utils import DistanceStrategy
+from load_llm.load_llm import LLMPretrained, LLMWrapper
 
-from retriever import Retriever
-
+from data_ingestion.retriever import Retriever
+from rag.rag import RAG
 EMBEDDING_MODEL_NAME = "thenlper/gte-small"
+READER_MODEL_NAME = "HuggingFaceH4/zephyr-7b-beta"
+
 import os
 os.environ["TOKENIZERS_PARALLELISM"] = "false"
 
@@ -23,14 +27,21 @@ embedding_model = HuggingFaceEmbeddings(
     encode_kwargs={"normalize_embeddings": True},  # Set `True` for cosine similarity
 )
 
+bnb_config = BitsAndBytesConfig(
+    load_in_4bit=True,
+    bnb_4bit_use_double_quant=True,
+    bnb_4bit_quant_type="nf4",
+    bnb_4bit_compute_dtype=torch.bfloat16,
+)
 
+model = AutoModelForCausalLM.from_pretrained(READER_MODEL_NAME, quantization_config=bnb_config)
+tokenizer = AutoTokenizer.from_pretrained(READER_MODEL_NAME)
 
 dataset = load_dataset("antoinejeannot/jurisprudence", "tribunal_judiciaire")
+
 RAW_KNOWLEDGE_BASE = [
     LangchainDocument(page_content=doc["text"], metadata={"source": doc["id"]}) for doc in tqdm(dataset['train'].take(100))
 ]
-
-
 
 text_splitter = RecursiveCharacterTextSplitter(
     chunk_size=512,  # The maximum number of characters in a chunk: we selected this value arbitrarily
@@ -41,16 +52,6 @@ text_splitter = RecursiveCharacterTextSplitter(
 
 retriever = Retriever(embedding_model=embedding_model, text_splitter=text_splitter, vector_store_path="faiss_index")
 
-# docs = []
-# for doc in RAW_KNOWLEDGE_BASE:
-#     docs += text_splitter.split_documents([doc])
+rag = RAG(vector_store=retriever.vector_store, model_name=LLMPretrained.TINY_LLAMA)
 
-
-tokenizer = AutoTokenizer.from_pretrained("thenlper/gte-small")
-
-
-user_query = "Aux termes de leurs dernières conclusions notifiées le 23 décembre 2022 par voie électronique, et au visa des articles 1792 et suivants"
-retrieved_docs = retriever.vector_store.similarity_search(query=user_query, k=5)
-for doc in retrieved_docs:
-    print(doc)
-    print("-----------------------------------------")
+rag.generate_answer(k=5, query="Qu'est-ce qu'un litige?", model=model, tokenizer=tokenizer)
--- src/rag/rag.py
+++ src/rag/rag.py
@@ -1,11 +1,88 @@
 from langchain_community.document_loaders import UnstructuredHTMLLoader
+from load_llm.load_llm import LLMPretrained, LLMWrapper
+from langchain.docstore.document import Document
+from transformers import pipeline
 
-class RAG():
-    def __init__(self, data_path: str) -> None:
-        self.documents = []
-        self.html_loader = UnstructuredHTMLLoader()
+class RAG:
+    def __init__(self, vector_store, model_name: LLMPretrained) -> None:
+        self.vector_store = vector_store
+        self.model = LLMWrapper(llm_pretrained=model_name)
+
+    def retrieve_k_top(self, k:int, query: str):
+        return self.vector_store.similarity_search(query=query, k=k)
+    
+    def get_formatted_prompt(self, query: str, retrieved_texts: list[str]):
+        PROMPT_TEMPLATE = """
+        Using the information contained in the context,
+        give a comprehensive answer to the question.\n
+        Respond only to the question asked, response should be concise and relevant to the question.
+        Provide the number of the source document when relevant.
+        If the answer cannot be deduced from the context, do not give an answer.\n
+        Context:\n
+        {context}\n
+        ---\n
+        Now here is the question you need to answer.\n
+
+        Question: {question}\n"""
+        context = ""
+        context += "".join([f"Document {str(i)}:::\n" + doc + "\n" for i, doc in enumerate(retrieved_texts)])
+
+        return PROMPT_TEMPLATE.format(question=query, context=context)   
+    
+    # def generate_answer(self, k:int, query: str):
+    #     retrieved_docs = self.retrieve_k_top(k, query)
+    #     retrieved_docs_text = [doc.page_content for doc in retrieved_docs]
+    #     prompt = self.get_formatted_prompt(query=query, retrieved_texts=retrieved_docs_text)
+    #     print('===========PROMPT=============')
+    #     print(prompt)
+    #     res = self.model.generate_text(prompt=prompt, max_length=1000)
+    #     print('===========RESULT=============')
+    #     print(res)
+
+    def generate_answer(self, k:int, query: str, model, tokenizer):
+
+        READER_LLM = pipeline(
+    model=model,
+    tokenizer=tokenizer,
+    task="text-generation",
+    do_sample=True,
+    temperature=0.2,
+    repetition_penalty=1.1,
+    return_full_text=False,
+    max_new_tokens=500,
+)
         
+        prompt_in_chat_format = [
+    {
+        "role": "system",
+        "content": """Using the information contained in the context,
+give a comprehensive answer to the question.
+Respond only to the question asked, response should be concise and relevant to the question.
+Provide the number of the source document when relevant.
+If the answer cannot be deduced from the context, do not give an answer.""",
+    },
+    {
+        "role": "user",
+        "content": """Context:
+{context}
+---
+Now here is the question you need to answer.
 
-    def load_vector_store(self):
-        documents = self.loader.load()
-        return documents
+Question: {question}""",
+    },
+]
+        RAG_PROMPT_TEMPLATE = tokenizer.apply_chat_template(
+    prompt_in_chat_format, tokenize=False, add_generation_prompt=True
+)
+        retrieved_docs = self.retrieve_k_top(k, query)
+        retrieved_docs_text = [doc.page_content for doc in retrieved_docs]
+        context = "\nExtracted documents:\n"
+        context += "".join([f"Document {str(i)}:::\n" + doc for i, doc in enumerate(retrieved_docs_text)])
+        final_prompt = RAG_PROMPT_TEMPLATE.format(question=query, context=context)
+        print('===========PROMPT=============')
+        print(final_prompt)
+        answer = READER_LLM(final_prompt)[0]["generated_text"]
+        print('===========RESULT=============')
+        print(answer)
+
+
